df00505633b88cdc0a22711353ac9c269023a3f0
Mikael Fredriksson <224Mikael@gmail.com>
2019-08-27 11:55:58 +0200

    Basic random methods with JUnit tests

---
+++ src/main/java/com/example/com/random_generator/Main.java
@@ -0,0 +1,16 @@
+package com.example.com.random_generator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyRandomMethods rand = new MyRandomMethods();
+		
+		System.out.println(rand.randomBetween1And10());
+		System.out.println(rand.randomBetween11And20());
+		System.out.println(rand.randomBetween21And30());
+		System.out.println(rand.randomBetween31And40());
+		System.out.println(rand.randomBetween41And50());
+	}
+
+}
---
+++ src/test/java/com/example/com/random_generator/MyRandomMethodsTest.java
@@ -0,0 +1,73 @@
+package com.example.com.random_generator;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class MyRandomMethodsTest {
+	MyRandomMethods methods = new MyRandomMethods();
+	
+	int rand = 0;
+	
+	@Test
+	public void randomBetween1And10Test() {
+		for (int i = 0; i < 1000; i++) {
+			rand = methods.randomBetween1And10();
+			
+			if (rand > 0 && rand < 11)
+				assertTrue(true);
+			else
+				assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void randomBetween11And20Test() {
+		for (int i = 0; i < 1000; i++) {
+			rand = methods.randomBetween11And20();
+			
+			if (rand > 10 && rand < 21)
+				assertTrue(true);
+			else
+				assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void randomBetween21And30Test() {
+		for (int i = 0; i < 1000; i++) {
+			rand = methods.randomBetween21And30();
+			
+			if (rand > 20 && rand < 31)
+				assertTrue(true);
+			else
+				assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void randomBetween31And40Test() {
+		for (int i = 0; i < 1000; i++) {
+			rand = methods.randomBetween31And40();
+			
+			if (rand > 30 && rand < 41)
+				assertTrue(true);
+			else
+				assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void randomBetween41And50Test() {
+		for (int i = 0; i < 1000; i++) {
+			rand = methods.randomBetween41And50();
+			
+			if (rand > 40 && rand < 51)
+				assertTrue(true);
+			else
+				assertTrue(false);
+		}
+	}
+}
